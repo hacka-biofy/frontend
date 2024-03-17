e908a9c0e37ea7669b7bbfb645ba509aa9a2d46c
netohd <toninetocontato@gmail.com>
2024-03-17 03:14:07 -0300

    improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,20 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import { AvatarMenu } from "@/components/AvatarMenu";
 import logo from "../../public/logoverde.png";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/components/ui/use-toast";
+
+import { LucideMessageCircleWarning } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { ToastAction } from "@/components/ui/toast";
 
 export default function Component() {
   const [selectedImage, setSelectedImage] = useState(null);
-  const { toast } = useToast()
+  const { toast } = useToast();
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -20,17 +26,24 @@ export default function Component() {
     if (selectedImage) {
       const formData = new FormData();
       formData.append("image", selectedImage);
-      // notify('Imagem enviada!');
 
       fetch("/upload", {
         method: "POST",
         body: formData,
       })
         .then((response) => {
+          //lógica
           toast({
             title: "Sucesso!",
             description: "Sua imagem foi enviada",
-          })
+            action: (
+              <ToastAction altText="Fechar notificação">Fechar</ToastAction>
+            ),
+          });
+
+          setTimeout(() => {
+            setSelectedImage(null);
+          }, 2000);
         })
         .catch((error) => {
           console.error("Error uploading image:", error);
@@ -44,10 +57,26 @@ export default function Component() {
         <header className="bg-zinc-800 border-b p-4">
           <div className="container mx-auto flex justify-center gap-4">
             <Image alt={"logo"} src={logo} width={100} className="" />
+            <AvatarMenu />
           </div>
         </header>
-        <main className="flex-1 flex items-center justify-center p-4">
-          <div className="max-w-sm w-full space-y-4 m-12">
+        <main className="flex-1 flex flex-col items-center p-4">
+          <div className="max-w-sm w-full space-y-4 m-4">
+            <Alert variant="default" className="bg-amber-400">
+              <div className="m-2">
+                <div className="flex flex-row">
+                  <AlertTitle className="text-gray-900 font-medium text-lg mb-2">
+                    {"Alerta"}
+                  </AlertTitle>
+                  <LucideMessageCircleWarning className="mx-1 w-4" />
+                </div>
+                <AlertDescription className="text-gray-800 mb-2">
+                  Não faça o envio de fotos desfocadas ou de qualidade inferior
+                  à 360p, mantendo assim a performance na predição.
+                </AlertDescription>
+              </div>
+            </Alert>
+
             <div className="flex items-center justify-center border border-dashed border-gray-200 rounded-lg w-full h-60 relative">
               <input
                 aria-label="Upload"
@@ -56,10 +85,12 @@ export default function Component() {
                 onChange={handleImageChange}
               />
               {selectedImage ? (
-                <img
+                <Image
                   src={URL.createObjectURL(selectedImage)}
                   alt="Selected"
                   className="object-cover w-full h-full rounded-lg"
+                  width={200}
+                  height={200}
                 />
               ) : (
                 <ImageIcon className="w-12 h-12" />
@@ -86,18 +117,14 @@ export default function Component() {
                 </>
               ) : (
                 <>
-                  <div>
+                  <div className="w-full relative">
                     <input
                       aria-label="Upload"
                       className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
                       type="file"
                       onChange={handleImageChange}
                     />
-                    <Button
-                      className="w-full bg-secondary"
-                      variant="outline"
-                      // onClick={handleImageChange}
-                    >
+                    <Button className="w-full bg-secondary" variant="outline">
                       Escolher da Galeria
                     </Button>
                   </div>
---
+++ src/components/AvatarMenu.tsx
@@ -0,0 +1,44 @@
+import { DropdownMenu } from '@radix-ui/react-dropdown-menu';
+import { UserIcon, LogOutIcon, SettingsIcon } from 'lucide-react';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function AvatarMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="ml-12 group p-0 relative focus:bg-transparent data-[state=open]:bg-transparent cursor-pointer">
+        <Avatar className='bg-slate-600 text-red-600 '>
+          <AvatarImage alt="" />
+          <AvatarFallback className='bg-zinc-600'>
+            <UserIcon className="text-secondary"></UserIcon>
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 mx-4">
+        <div className="flex flex-col items-center p-4">
+          
+          <DropdownMenuLabel className="select-none">Fulano</DropdownMenuLabel>
+          <DropdownMenuLabel className="select-none text-gray-500 font-normal">
+            biofy
+          </DropdownMenuLabel>
+        </div>
+        <DropdownMenuSeparator className="bg-gray-200" />
+        <DropdownMenuItem onClick={() => { console.log('Hello') }}>
+          <SettingsIcon className="w-4 h-4 mr-4 text-gray-700" />
+          Configurações
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => { console.log('Hello') }}>
+          <LogOutIcon className="w-4 h-4 mr-4 text-gray-700" />
+          Sair
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
